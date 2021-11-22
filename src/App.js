1a1033fb57304fcabeb91c0baca4e4dfdc356c45
benpaquier <hello@benpaquier.com>
2021-11-22 11:06:32 +0100

    Fix

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { ChakraProvider } from "@chakra-ui/provider"
 import { Flex, Box, Center } from '@chakra-ui/react'
 
 import { MapContextProvider } from "./contexts/Map"
@@ -6,16 +7,18 @@ import List from './components/List'
 
 const App = () => {
   return (
-    <MapContextProvider>
-      <Flex h="100vh" w="100%">
-        <Box h="100%" w="30%" p={5} overflowY="scroll">
-          <List />
-        </Box>
-        <Center background="teal" h="100%" w="70%">
-          <Map />
-        </Center>
-      </Flex>
-    </MapContextProvider>
+    <ChakraProvider>
+      <MapContextProvider>
+        <Flex h="100vh" w="100%">
+          <Box h="100%" w="30%" p={5} overflowY="scroll">
+            <List />
+          </Box>
+          <Center background="teal" h="100%" w="70%">
+            <Map />
+          </Center>
+        </Flex>
+      </MapContextProvider>
+    </ChakraProvider>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import { ChakraProvider } from "@chakra-ui/provider"
+import { ColorModeScript } from "@chakra-ui/react"
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import theme from './theme'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
+  <>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <App />
+  </>,
   document.getElementById('root')
 );
 
